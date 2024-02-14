ef090300034f3971f379d7454c7e187d29853918
Niyi0904 <owoyeminiyi2@gmail.com>
2024-02-14 05:56:34 -0800

    update our menu-item to navigate useNavigate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom'; 
+
+import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = (props) => {
+  console.log(props);
+  return (<div>
+    <h1>HATS PAGE</h1>
+  </div>
+)};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path='/' Component={HomePage} />
+        <Route path='/hats' Component={HatsPage} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+import './shop.styles.scss';
+
+class Shoppage extends React.Component {
+  constructor() {
