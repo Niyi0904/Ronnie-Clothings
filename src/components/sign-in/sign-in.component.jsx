e2b43e1c09aa4dc25d5e805437c34e8617b6874f
Niyi0904 <owoyeminiyi2@gmail.com>
2025-05-06 05:23:00 +0100

    test

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -30,7 +30,7 @@ class SignIn extends React.Component{
 
   render() {
     return(
-      <div className='sign-in'>
+      <div className='sign-in relative top-24'>
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
